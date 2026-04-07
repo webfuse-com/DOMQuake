537f7da6fbb463d98716809b0e1af025e196b593
t-ski <t.schiepanski@gmx.de>
2026-04-07 10:56:25 +0200

    Extend event emitter class with listener wildcard

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,8 +1,12 @@
 import { type EventArgument } from "./types.ts";
 
 
+const WILDCARD_EVENT_NAME = "*" as const;
+
+
+type Event<T> = T | typeof WILDCARD_EVENT_NAME;
 type ListenerCb = (...args: unknown[]) => void;
-type ListenerMap<T> = Map<T, ListenerCb[]>;
+type ListenerMap<T> = Map<Event<T>, ListenerCb[]>;
 
 
 export class EventEmitter<T> {
@@ -14,21 +18,20 @@ export class EventEmitter<T> {
         once: new Map<T, ListenerCb[]>(),
     };
 
-    private getListeners(listenerMap: ListenerMap<T>, event: T): ListenerCb[] {
-        !listenerMap.has(event)
-            && listenerMap.set(event, []);
+    private getListeners(listenerMap: ListenerMap<T>, event: Event<T>): ListenerCb[] {
+        !listenerMap.has(event) && listenerMap.set(event, []);
 
         return listenerMap.get(event)!;
     }
 
-    public on(event: T, listener: ListenerCb): this {
+    public on(event: Event<T>, listener: ListenerCb): this {
         this.getListeners(this.listeners.on, event)
             .push(listener);
 
         return this;
     }
 
-    public once(event: T, listener: ListenerCb): this {
+    public once(event: Event<T>, listener: ListenerCb): this {
         this.getListeners(this.listeners.once, event)
             .push(listener);
 
@@ -38,9 +41,14 @@ export class EventEmitter<T> {
     public emit<A = EventArgument>(event: T, arg: A) {
         this.getListeners(this.listeners.once, event)
             .forEach((listener: ListenerCb) => listener(arg));
+        this.getListeners(this.listeners.once, WILDCARD_EVENT_NAME)
+            .forEach((listener: ListenerCb) => listener(event, arg));
         this.getListeners(this.listeners.on, event)
             .forEach((listener: ListenerCb) => listener(arg));
+        this.getListeners(this.listeners.on, WILDCARD_EVENT_NAME)
+            .forEach((listener: ListenerCb) => listener(event, arg));
 
         this.listeners.once.set(event, []);
+        this.listeners.once.set(WILDCARD_EVENT_NAME, []);
     }
 }
--- test/unit/EventEmitter.test.js
+++ test/unit/EventEmitter.test.js
@@ -5,7 +5,7 @@ const eventEmitter = new EventEmitter();
 
 const resultCollection = [];
 
-eventEmitter.emit("foo");
+eventEmitter.emit("foo", 0);
 
 eventEmitter.on("foo", (arg) => resultCollection.push([ "foo", "on", arg ]));
 eventEmitter.on("bar", (arg) => resultCollection.push([ "bar", "on", arg ]));
@@ -13,6 +13,10 @@ eventEmitter.on("bar", (arg) => resultCollection.push([ "bar", "on", arg ]));
 eventEmitter.once("foo", (arg) => resultCollection.push([ "foo", "once", arg ]));
 
 eventEmitter.emit("foo", 1);
+
+eventEmitter.on("*", (event, arg) => resultCollection.push([ "*", event, "on", arg ]));
+eventEmitter.once("*", (event, arg) => resultCollection.push([ "*", event, "once", arg ]));
+
 eventEmitter.emit("foo", 2);
 
 eventEmitter.emit("bar", 3);
@@ -20,10 +24,13 @@ eventEmitter.emit("bar", 3);
 assertEqual(
     new Set(resultCollection),
     new Set([
-        [ "foo", "once", 1 ],
-        [ "foo", "on", 1 ],
-        [ "foo", "on", 2 ],
-        [ "bar", "on", 3 ]
+        [ 'foo', 'once', 1 ],
+        [ 'foo', 'on', 1 ],
+        [ '*', 'foo', 'once', 2 ],
+        [ 'foo', 'on', 2 ],
+        [ '*', 'foo', 'on', 2 ],
+        [ 'bar', 'on', 3 ],
+        [ '*', 'bar', 'on', 3 ]
     ]),
     "Incorrect result collection"
 );
